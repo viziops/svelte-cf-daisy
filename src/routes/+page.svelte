315f47e2be3fa3bfee9906a876d9db4b3e021db9
Edwin <dev@viziops.com>
2023-01-08 22:09:30 +0200

    button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-<h1 class="text-2xl font-bold underline">I wonder if this will work?</h1>
+<button class="btn btn-primary">Button</button>
