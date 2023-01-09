398ff3d23febc7be736c3653bf0d8827fad44a2a
Edwin <dev@viziops.com>
2023-01-09 16:06:16 +0200

    prismicio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,16 @@
-<button class="btn btn-primary">Button</button>
+<script>
+	import createClient from '$lib/prismicio';
+	import * as prismicH from '@prismicio/helpers';
+
+	const client = createClient();
+	const prismicQuery = client.getFirst();
+</script>
+
+{#await prismicQuery}
+	<p>Loading...</p>
+{:then prismicResponse}
+	<h1>{prismicH.asText(prismicResponse.data.title)}</h1>
+	{@html prismicH.asHTML(prismicResponse.data.description)}
+{:catch error}
+	<pre>{error.message}</pre>
+{/await}
